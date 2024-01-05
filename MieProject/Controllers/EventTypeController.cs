5f68c37fd607a48a84f7f949ba0207b42c369c4d
swalame123 <srikanth.n@congruentglobal.com>
2024-01-05 13:26:45 +0530

    updated event-type

--- MieProject/Controllers/EventTypeController.cs
+++ MieProject/Controllers/EventTypeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Smartsheet.Api;
+using Smartsheet.Api.Models;
 
 namespace MieProject.Controllers
 {
@@ -15,6 +17,45 @@ namespace MieProject.Controllers
             this.configuration = configuration;
             accessToken = configuration.GetSection("SmartsheetSettings:AccessToken").Value;
 
+        }
+        [HttpGet("GetEventData")]
+        public IActionResult GetEventData()
+        {
+            try
+            {
+                SmartsheetClient smartsheet = new SmartsheetBuilder().SetAccessToken(accessToken).Build();
+                string sheetId = configuration.GetSection("SmartsheetSettings:EventType").Value;
+                long.TryParse(sheetId, out long parsedSheetId);
+                Sheet sheet = smartsheet.SheetResources.GetSheet(parsedSheetId, null, null, null, null, null, null, null);
+                List<Dictionary<string, object>> sheetData = new List<Dictionary<string, object>>();
+                List<string> columnNames = new List<string>();
+                foreach (Column column in sheet.Columns)
+                {
+                    columnNames.Add(column.Title);
+                }
+                foreach (Row row in sheet.Rows)
+                {
+                    Dictionary<string, object> rowData = new Dictionary<string, object>();
+                    for (int i = 0; i < row.Cells.Count && i < columnNames.Count; i++)
+                    {
+                        if (columnNames[i] == "EventTypeId")
+                        {
+                            rowData[columnNames[i]] = row.Cells[i].Value;
+                        }
+                        if (columnNames[i] == "EventType")
+                        {
+                            rowData[columnNames[i]] = row.Cells[i].Value;
+                        }
+
+                    }
+                    sheetData.Add(rowData);
+                }
+                return Ok(sheetData);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
--- MieProject/Controllers/LoginAndRegisterController.cs
+++ MieProject/Controllers/LoginAndRegisterController.cs
@@ -39,7 +39,7 @@ namespace MieProject.Controllers
         }
     
 
-        [HttpGet("GetSheetDataKeyValue")]
+        [HttpGet("GetSheetData")]
 
         public IActionResult GetSheetData()
         {
