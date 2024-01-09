6cf801d4b0e522da285fd7c65e879387defa65d0
swalame123 <srikanth.n@congruentglobal.com>
2024-01-09 17:14:55 +0530

    test

---
+++ MieProject/Controllers/Testing/TestController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Smartsheet.Api;
+using Smartsheet.Api.Models;
+
+namespace MieProject.Controllers.Testing
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly string accessToken;
+        private readonly IConfiguration configuration;
+
+        public TestController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            accessToken = configuration.GetSection("SmartsheetSettings:AccessToken").Value;
+
+        }
+        [HttpGet("GetApprovedSpeakersData")]
+        public IActionResult GetApprovedSpeakersData()
+        {
+            try
+            {
+                SmartsheetClient smartsheet = new SmartsheetBuilder().SetAccessToken(accessToken).Build();
+                string sheetId = configuration.GetSection("SmartsheetSettings:TestApprovedSpeakers").Value;
+                long.TryParse(sheetId, out long parsedSheetId);
+                Sheet sheet = smartsheet.SheetResources.GetSheet(parsedSheetId, null, null, null, null, null, null, null);
+                List<Dictionary<string, object>> sheetData = new List<Dictionary<string, object>>();
+                List<string> columnNames = new List<string>();
+                foreach (Column column in sheet.Columns)
+                {
+                    columnNames.Add(column.Title);
+                }
+                foreach (Row row in sheet.Rows)
+                {
+                    Dictionary<string, object> rowData = new Dictionary<string, object>();
+                    for (int i = 0; i < row.Cells.Count && i < columnNames.Count; i++)
+                    {
+                        rowData[columnNames[i]] = row.Cells[i].Value;
+
+                    }
+                    sheetData.Add(rowData);
+                }
+                return Ok(sheetData);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
